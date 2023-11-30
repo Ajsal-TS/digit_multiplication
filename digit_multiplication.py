ebe6a6c5764cba94df2837666886b07c4fb1dac6
Ajsal-TS <ajsal.ts@neoito.com>
2023-11-30 06:54:54 +0530

    added docstring.

--- digit_multiplication.py
+++ digit_multiplication.py
@@ -1,7 +1,11 @@
 import math
 def calc(word,lst):
-    list1=list(str(word))
-    element=[eval(i) for i in list1]
+    """Takes any whole number as input and then outputs the result of each step, 
+    starting with the input itself, until we hit a single digit.
+    :param word:user input.
+    :param lst:to print the output list.
+    """
+    element=[eval(i) for i in list(str(word))]
     if len(element)==1:
         print(lst)
         return
